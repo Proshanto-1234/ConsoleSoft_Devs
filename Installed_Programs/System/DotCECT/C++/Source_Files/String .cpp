73ce7cc5f74d6661e53e58f8a4fe188be6095003
Proshanto-1234 <proshantobrahmmachary@outlook.com>
2025-02-16 18:17:02 +0600

    Update String .cpp

--- Installed_Programs/System/DotCECT/C++/Source_Files/String .cpp
+++ Installed_Programs/System/DotCECT/C++/Source_Files/String .cpp
@@ -1,20 +1,27 @@
 #include "../Header_Files/String.h"
-String::String(int l)  {
-str = new char [l + 1];
-*str = NULL;
-length=0;
-maxlen=l;
-fail=false;
+using namespace std;
+String::String(int l) {
+    str = new char[l + 1];
+    str[0] = '\0';
+    length = 0;
+    maxlen = l;
+    fail = false;
 }
+
 String::~String() {
-delete [] str;
+    delete[] str;
 }
+
 void String::concat(int len) {
-  char *tstr = new char char[length + len + 1];
-int i = 0;
-while (str[i]) tstr[i] = str[i++];
-tstr[i] = NULL;
-delete [] str;
-str = tstr;
-maxlen = length + len;
+    char* tstr = new char[length + len + 1];
+    int i = 0;
+    while (str[i]) tstr[i] = str[i++];
+    tstr[i] = '\0';
+    delete[] str;
+    str = tstr;
+    maxlen = length + len;
 }
+void String::Space(int i) {
+  if (i) {
+      maxlen = strlen(str)-i;
+      memmove(
