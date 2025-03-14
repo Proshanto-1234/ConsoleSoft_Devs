5651e11b76961566ba0caa6862cc3624e4e7146d
Proshanto-1234 <proshantobrahmmachary@outlook.com>
2025-03-14 13:19:32 +0600

    Create cstring.cpp

---
+++ Installed_Programs/System/DotCECT/C++/Source_Files/cstring.cpp
@@ -0,0 +1,3 @@
+#include "cstring.h"
+/* ConsoleSoft equivalent of Windows cstring
+* Functions defined here with prefix "_s" are safe functions, like str_cpy_s, while functions defined here with prefix "_s" are unsafe (USE AT YOUR OWN RISKS)
